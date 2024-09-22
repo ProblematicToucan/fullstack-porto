f2c4f6d697490ab979c5b1bd02a49dfed9bc27d0
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2024-09-23 06:53:06 +0700

    feat: Add project media models

---
+++ app/Models/ProjectMedia.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class ProjectMedia extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'project_id',
+        'media_type',
+        'media_url',
+        'media_description'
+    ];
+}
