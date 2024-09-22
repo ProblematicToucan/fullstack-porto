5af8ac71c8c470988970c57098eac8d6ad23b21e
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2024-09-22 15:56:45 +0700

    feat: Init filament

--- vite.config.js
+++ vite.config.js
@@ -1,12 +1,15 @@
 import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
+import laravel, { refreshPaths } from 'laravel-vite-plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
     plugins: [
         laravel({
             input: 'resources/js/app.tsx',
-            refresh: true,
+            refresh: [
+                ...refreshPaths,
+                'app/**'
+            ],
         }),
         react(),
     ],
