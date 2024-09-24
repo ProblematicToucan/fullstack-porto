a87d114b4d09d5a1a999df8563c193a02225afd3
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2024-09-24 14:01:05 +0700

    feat: Create landing page base

---
+++ resources/js/Pages/Landing.tsx
@@ -0,0 +1,11 @@
+import MainLayout from '@/Layouts/MainLayout'
+import { Head } from '@inertiajs/react'
+
+export default function Landing() {
+    return (
+        <MainLayout>
+            <Head title='Gamal Abdul Aziz' />
+            main landing page
+        </MainLayout>
+    )
+}
