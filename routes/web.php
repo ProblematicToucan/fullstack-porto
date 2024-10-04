c0dd3e5aadcfbed1e99095af8e90534fe1a7ad8e
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2024-10-04 19:56:13 +0700

    feat: Minify projects & delete unused class

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,5 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ProjectController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
@@ -18,15 +17,3 @@ Route::prefix('/project')->name('project.')->controller(ProjectController::class
 Route::get('/bio', function () {
     return Inertia::render('Profile');
 })->name('bio');
-
-// Route::get('/dashboard', function () {
-//     return Inertia::render('Dashboard');
-// })->middleware(['auth', 'verified'])->name('dashboard');
-
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-// });
-
-// require __DIR__ . '/auth.php';
