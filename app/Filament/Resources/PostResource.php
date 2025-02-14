392b90af22782ebde3d34c01cfdf111aef79e50e
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2025-02-14 11:15:18 +0700

    feat: Add Post Filament Resource

---
+++ app/Filament/Resources/PostResource.php
@@ -0,0 +1,133 @@
+<?php
+
+namespace App\Filament\Resources;
+
+use App\Filament\Resources\PostResource\Pages;
+use App\Models\Post;
+use Filament\Forms;
+use Filament\Forms\Form;
+use Filament\Resources\Resource;
+use Filament\Tables;
+use Filament\Tables\Table;
+use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Database\Eloquent\SoftDeletingScope;
+use Illuminate\Support\Str;
+
+class PostResource extends Resource
+{
+    protected static ?string $model = Post::class;
+
+    protected static ?string $navigationIcon = 'heroicon-o-rectangle-stack';
+
+    public static function form(Form $form): Form
+    {
+        return $form
+            ->schema([
+                Forms\Components\TextInput::make('title')
+                    ->required(),
+                Forms\Components\TextInput::make('slug')
+                    ->default(fn (): string => Str::uuid()->toString())
+                    ->disabled()
+                    ->dehydrated()
+                    ->required(),
+                Forms\Components\Toggle::make('is_public')
+                    ->required(),
+                Forms\Components\Section::make('Content')
+                    ->schema([
+                        Forms\Components\Builder::make('content')
+                            ->hiddenLabel(true)
+                            ->blocks([
+                                Forms\Components\Builder\Block::make('Paragraph')
+                                    ->schema([
+                                        Forms\Components\RichEditor::make('text')
+                                            ->disableToolbarButtons([
+                                                'attachFiles',
+                                            ])
+                                            ->hiddenLabel(),
+                                    ])
+                                    ->icon('heroicon-m-bars-3-bottom-left'),
+                                Forms\Components\Builder\Block::make('image')
+                                    ->schema([
+                                        Forms\Components\FileUpload::make('image')
+                                            ->hiddenLabel()
+                                            ->disk('r2')
+                                            ->directory('project-images')
+                                            ->visibility('private')
+                                            ->image()
+                                            ->imageEditor()
+                                    ])
+                                    ->icon('heroicon-m-photo'),
+                            ])
+                            ->required(),
+                    ]),
+            ]);
+    }
+
+    public static function table(Table $table): Table
+    {
+        return $table
+            ->columns([
+                Tables\Columns\TextColumn::make('title')
+                    ->searchable(),
+                Tables\Columns\TextColumn::make('slug')
+                    ->searchable(),
+                Tables\Columns\IconColumn::make('is_public')
+                    ->boolean(),
+                Tables\Columns\TextColumn::make('created_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('updated_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+                Tables\Columns\TextColumn::make('deleted_at')
+                    ->dateTime()
+                    ->sortable()
+                    ->toggleable(isToggledHiddenByDefault: true),
+            ])
+            ->filters([
+                Tables\Filters\TrashedFilter::make(),
+            ])
+            ->actions([
+                Tables\Actions\EditAction::make(),
+            ])
+            ->bulkActions([
+                Tables\Actions\BulkActionGroup::make([
+                    Tables\Actions\DeleteBulkAction::make(),
+                    Tables\Actions\ForceDeleteBulkAction::make(),
+                    Tables\Actions\RestoreBulkAction::make(),
+                ]),
+            ]);
+    }
+
+    public static function getRelations(): array
+    {
+        return [
+            //
+        ];
+    }
+
+    public static function getPages(): array
+    {
+        return [
+            'index' => Pages\ListPosts::route('/'),
+            'create' => Pages\CreatePost::route('/create'),
+            'edit' => Pages\EditPost::route('/{record}/edit'),
+        ];
+    }
+
+    public static function getEloquentQuery(): Builder
+    {
+        return parent::getEloquentQuery()
+            ->withoutGlobalScopes([
+                SoftDeletingScope::class,
+            ]);
+    }
+
+    public static function getNavigationBadge(): ?string
+    {
+        return static::$model::count();
+    }
+
+}
