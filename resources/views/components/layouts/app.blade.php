22911c28a308eec0759ecaf2fb362113a88fcfa3
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2025-02-28 14:03:50 +0700

    feat: Update Style On Blade

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -23,6 +23,11 @@
         {{ filled($title) ? "{$title} - " : null }} {{ $brandName }}
     </title>
 
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+
+    <!-- Scripts -->
     @viteReactRefresh
     @vite(['resources/js/app.js', 'resources/css/app.css'])
 
--- resources/views/livewire/auth/login.blade.php
+++ resources/views/livewire/auth/login.blade.php
@@ -40,8 +40,8 @@
                         <label for="remember" class="ml-2 text-sm font-medium ">Remember Me</label>
                     </div>
 
-                    <button type="submit"
-                        class="inline-flex items-center justify-center w-full h-10 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-teal-600 hover:bg-teal-500 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
+                    <button wire:loading.class="opacity-50" type="submit"
+                        class="inline-flex items-center justify-center w-full h-10 px-4 py-2 text-sm font-medium tracking-wide text-white transition-colors duration-200 rounded-md bg-primary-600 hover:bg-primary-500 focus:ring-2 focus:ring-offset-2 focus:ring-neutral-900 focus:shadow-outline focus:outline-none">
                         Login
                     </button>
                     <div class="relative py-6">
