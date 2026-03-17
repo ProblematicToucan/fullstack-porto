1ba9eeff0955d97ad72ab1654afa20c23fb49d54
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2026-03-17 19:42:36 +0700

    feat(n): controllers and routes for site pagesAdd Landing, Project, Post and About to centralize pagelogic and prepare data for views. Implement LandingController toserve the home page with featured projects and latest posts AddProjectController index and show actions loading categories andtech stacks, and PostController with index and show actions usingpublic scope and slug lookup. Add AboutController as an invokablecontroller to serve the about page.
    Register new routes in routes/web.php: replace the default welcomeroute with the LandingController as, add project, post and aboutroutes and wire them to their controllers. Paginate lists and eagerload relationships to performance and prepare forrendering.

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\View\View;
+
+class PostController extends Controller
+{
+    public function index(): View
+    {
+        $posts = Post::public()->latest()->paginate(10);
+
+        return view('posts.index', compact('posts'));
+    }
+
+    public function show(string $post): View
+    {
+        $post = Post::public()->where('slug', $post)->firstOrFail();
+
+        return view('posts.show', compact('post'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,17 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\LandingController;
+use App\Http\Controllers\PostController;
+use App\Http\Controllers\ProjectController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', LandingController::class)->name('home');
+
+Route::get('/project', [ProjectController::class, 'index'])->name('project.index');
+Route::get('/project/{project}', [ProjectController::class, 'show'])->name('project.show');
+
+Route::get('/post', [PostController::class, 'index'])->name('post.index');
+Route::get('/post/{post}', [PostController::class, 'show'])->name('post.show');
+
+Route::get('/about', AboutController::class)->name('about');
