3abd486cbe3470d595e085e87a4df302850438c1
Gamal Abdul Aziz <gamal.aziz1000@gmail.com>
2026-03-29 14:07:50 +0700

    refactor(AppServiceProvider): remove Vite navigation tracking attributes and streamline HTTPS enforcement in production

--- tests/Feature/ViteNavigateTrackTest.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-test('vite entry tags omit data navigate track reload for wire navigate performance', function () {
-    $html = $this->get('/')->assertSuccessful()->getContent();
-
-    expect($html)->not->toContain('data-navigate-track="reload"');
-});
